52710a817435f571a55e02888cfe8c0d81cc3576
Michael A. Chapman <chapmanm305@gmail.com>
2019-05-24 14:52:12 -0700

    Add functionality for inverting the bmp colors

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 
 const fs = require('fs');
 
+const FILE_TYPE_OFFSET = 0;
+const FILE_SIZE_OFFSET = 2;
+const PIXEL_OFFSET = 10;
+const WIDTH_OFFSET = 18;
+const HEIGHT_OFFSET = 22;
+const BYTES_PER_PIXEL_OFFSET = 28;
+const COLOR_TABLE_OFFSET = 54;
+
 /**
  * Bitmap -- receives a file name, used in the transformer to note the new buffer
  * @param filePath
@@ -17,8 +25,13 @@ function Bitmap(filePath) {
  */
 Bitmap.prototype.parse = function(buffer) {
   this.buffer = buffer;
-  this.type = buffer.toString('utf-8', 0, 2);
-  //... and so on
+  this.type = buffer.toString('utf-8', FILE_TYPE_OFFSET, 2);
+  this.fileSize = buffer.readInt32LE(FILE_SIZE_OFFSET);
+  this.pixelOffset = buffer.readInt32LE(PIXEL_OFFSET);
+  this.width = buffer.readInt32LE(WIDTH_OFFSET);
+  this.heigt = buffer.readInt32LE(HEIGHT_OFFSET);
+  this.bytesPerPixel = buffer.readInt32LE(BYTES_PER_PIXEL_OFFSET);
+  this.colorArray = buffer.slice(COLOR_TABLE_OFFSET, this.pixelOffset);
 };
 
 /**
@@ -38,18 +51,31 @@ Bitmap.prototype.transform = function(operation) {
  * @param bmp
  */
 const transformGreyscale = (bmp) => {
-
   console.log('Transforming bitmap into greyscale', bmp);
 
   //TODO: Figure out a way to validate that the bmp instance is actually valid before trying to transform it
-
+  // console.log(bmp);
   //TODO: alter bmp to make the image greyscale ...
-
+  for (let i = 0; i < bmp.colorArray.length; i+=4) {
+    let gray = (bmp.colorArray[i] + bmp.colorArray[i+1] + bmp.colorArray[i+2]);
+    bmp.colorArray[i] = gray;
+    bmp.colorArray[i+1] = gray;
+    bmp.colorArray[i+2] = gray;
+  }
 };
 
 const doTheInversion = (bmp) => {
-  bmp = {};
-}
+  console.log('Transforming bitmap into greyscale', bmp);
+
+  //TODO: Figure out a way to validate that the bmp instance is actually valid before trying to transform it
+  // console.log(bmp);
+  //TODO: alter bmp to make the image greyscale ...
+  for (let i = 0; i < bmp.colorArray.length; i+=4) {
+    bmp.colorArray[i] = bmp.colorArray[i+1];
+    bmp.colorArray[i+1] = bmp.colorArray[i+2];
+    bmp.colorArray[i+2] = bmp.colorArray[i];
+  }
+};
 
 /**
  * A dictionary of transformations
@@ -57,7 +83,7 @@ const doTheInversion = (bmp) => {
  */
 const transforms = {
   greyscale: transformGreyscale,
-  invert: doTheInversion
+  invert: doTheInversion,
 };
 
 // ------------------ GET TO WORK ------------------- //
